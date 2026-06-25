8152ee0256b78d92433e9c3107ddbb8864832880
Gaizka <chocarrovaz.gaizka@gmail.com>
2026-06-25 11:36:54 +0200

    Modificacion en AdminCars y prueba despliegue

--- src/components/admin/Pages/cars/AdminCars.jsx
+++ src/components/admin/Pages/cars/AdminCars.jsx
@@ -5,6 +5,7 @@ import { CarContainer } from '../../components/car/CarContainer'
 export const AdminCars = () => {
   return (
     <main className="main-content">
+      <h1> Administrador de coches</h1>
       <Link to='/admin/cars/create'>añadir maricoche</Link>
       <CarContainer/>
     </main>
