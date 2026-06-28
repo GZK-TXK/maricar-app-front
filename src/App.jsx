fe3919c9827e1ebbf0517bab4302c68196d16370
Gaizka <chocarrovaz.gaizka@gmail.com>
2026-06-28 22:13:04 +0200

    Logo ahora enlaza con homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.scss'
 import { AppRoutes } from './routes/AppRoutes.jsx'
 import { NavBarPublic } from './components/public/components/NavBarPublic.jsx'
 import { AuthProvider } from './AuthContext'
+import { NavLink } from 'react-router'
 
 export const App = () => {
   return (
     <AuthProvider>
       <header>
-        <img src="/logo.png" alt="MariCar App" className="logo" />
+        <NavLink to="/"><img src="/logo.png" alt="MariCar App" className="logo" /></NavLink>
         <NavBarPublic />
       </header>
       <AppRoutes />
